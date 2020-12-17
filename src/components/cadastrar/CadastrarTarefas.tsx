59eed7e1167b57927f4c23f89843b3ee7d68ba5a
Paulo Roberto <prmorais1302@gmail.com>
2020-12-17 17:09:57 -0300

    Criando o formulario de cadastro

--- src/components/cadastrar/CadastrarTarefas.tsx
+++ src/components/cadastrar/CadastrarTarefas.tsx
@@ -1,8 +1,41 @@
+import { A } from 'hookrouter';
 import React from 'react';
+import { Button, Form, Jumbotron } from 'react-bootstrap';
 
 const CadastrarTarefas: React.FC = () => {
   return (
-    <h1>CadastrarTarefas</h1>
+    <div>
+      <h3 className="text-center">Cadastrar</h3>
+      <Jumbotron>
+        <Form>
+          <Form.Group>
+            <Form.Label>Tarefas</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Digite uma tarefa"
+              minLength={5}
+              maxLength={100}
+              required
+            />
+
+            <Form.Control.Feedback type="invalid" >
+              A tarefa deve conter ao menos 5 caracteres.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="text-center" >
+            <Button
+              variant="success"
+              type="submit"
+            >
+              Cadastrar
+            </Button>
+            &nbsp;
+            <A href="/" className="btn btn-light">Voltar</A>
+          </Form.Group>
+        </Form>
+      </Jumbotron>
+    </div>
   );
 };
 
