343736b45bbb1b0822bb97ccc12b3fad25c35c82
Paulo Roberto <prmorais1302@gmail.com>
2020-12-17 17:31:14 -0300

    Criando a modal do formulario de cadastro

--- src/components/cadastrar/CadastrarTarefas.tsx
+++ src/components/cadastrar/CadastrarTarefas.tsx
@@ -1,6 +1,6 @@
 import { A } from 'hookrouter';
 import React from 'react';
-import { Button, Form, Jumbotron } from 'react-bootstrap';
+import { Button, Form, Jumbotron, Modal } from 'react-bootstrap';
 
 const CadastrarTarefas: React.FC = () => {
   return (
@@ -34,6 +34,20 @@ const CadastrarTarefas: React.FC = () => {
             <A href="/" className="btn btn-light">Voltar</A>
           </Form.Group>
         </Form>
+
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Sucesso</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            Tarefa adicionada com sucesso!
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="success">Continuar</Button>
+          </Modal.Footer>
+        </Modal>
       </Jumbotron>
     </div>
   );
